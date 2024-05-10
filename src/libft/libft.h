3906d9d215264d2bda5faf4503de8d0fccc14e3c
howset <howsetya@googlemail.com>
2024-05-10 21:42:04 +0200

    Add ft_atol

--- src/libft/libft.h
+++ src/libft/libft.h
@@ -51,4 +51,5 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+long	ft_atol(const char *nptr);
 #endif
